4d9eae2a35fdbf7cd64f5ddbefcd6cc5ccb3c4fb
ferkopar <ferkopa@gmail.com>
2015-05-03 07:52:36 +0200

    default

---
+++ sql/c_0503_002_GEN_SUB_TREATM.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION OBH_TEST.GEN_SUB_TREATM (p_treatm_id number)
+  RETURN number
+AS
+  cursor params is
+    select * from TREATM_PARAM where TREATM_ID = p_treatm_id;
+  l_treatm_row treatm%rowtype;
+  l_new_treatm_id number(12,0);
+  l_new_doc_id  number(12,0);
+  l_norma_id NUMBER(12,0);
+  l_norma_doc_id NUMBER(12,0);
+  l_dummy NUMBER(12,0);
+
+BEGIN
+  
+
+  SELECT treatm_id2 into l_norma_id FROM TREATM_REL WHERE TREATM_ID1 = p_treatm_id and REL_TYPE_ID =  1308590116;
+  l_new_treatm_id := COPY_TREATM_AND_PARAMS(p_treatm_id);
+  l_dummy := INSERT_TREATM_SUB(l_new_treatm_id,p_treatm_id);
+  l_dummy := INSERT_TREATM_N_K_REL(l_norma_id,l_new_treatm_id);
+--  begin
+--    SELECT treatm_id1 INTO l_norma_doc_id FROM TREATM_REL WHERE TREATM_ID2 = l_norma_id AND REL_TYPE_ID = 1308590191;
+--    l_new_doc_id := COPY_TREATM_AND_PARAMS(l_norma_doc_id);
+--    l_dummy := INSERT_TREATM_DOC_REL(l_new_treatm_id,l_new_doc_id);
+--  exception when others then
+--    null;
+--  end;
+  
+  return l_new_treatm_id;
+  
+END;
+/
