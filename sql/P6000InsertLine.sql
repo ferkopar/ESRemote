9c6940d6b85a11e4e2ae2d0269d18975ebca9d90
ferkopar <ferkopa@gmail.com>
2015-06-05 16:35:11 +0200

    delete old

--- sql/P6000InsertLine.sql
+++
@@ -1,6 +0,0 @@
-declare 
-   l_lista_id varchar(255);
-BEGIN
-   l_lista_id := wwv_flow.g_x01; 
-   generate_html.INSERT_EMPTY_LINE(:P6000_DATA_INCO_ID, :P6000_LISTA_ID, :P6000_DOC_ID);
-END;
--- sql/c_0504_004_OBH_TEST_GET_NORMA_DOC_ID.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE OR REPLACE FUNCTION OBH_TEST.GET_NORMA_DOC_ID (p_norma_id NUMBER)
-  RETURN NUMBER
-AS
-  l_norma_doc_id NUMBER(12,0);
-BEGIN
-  SELECT treatm_id1 INTO l_norma_doc_id FROM TREATM_REL WHERE TREATM_ID2 = p_norma_id AND REL_TYPE_ID = 1308590191;
-  RETURN l_norma_doc_id;
-END;
-/
