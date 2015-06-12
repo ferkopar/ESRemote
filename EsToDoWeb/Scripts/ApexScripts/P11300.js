a143aa5e0e7731dd740f1760bcc8aac30e0f5d9a
ferkopar <ferkopa@gmail.com>
2015-06-12 22:33:50 +0200

    asas

---
+++ EsToDoWeb/Scripts/ApexScripts/P11300.js
@@ -0,0 +1,13 @@
+﻿jQuery.fn.tagName = function () {
+    return this.prop("tagName");
+};
+
+function openTree(p) {
+    listItem = $(p).closest('li');
+    var d = document.createElement('div');
+    listItem.append(d);
+
+    txt = document.createTextNode('This is the text in new element.');
+    d.appendChild(txt);
+}
+
--- EsToDoWeb/Scripts/ApexScripts/p410.js
+++ EsToDoWeb/Scripts/ApexScripts/p410.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(document).ready(function () {
+    setInputElementsWidth();
+});
+
+function setInputElementsWidth() {
+    $("#TreatmTable input").each(function () {
+       $(this).width( $(this).closest('td').width());
+    });
+
+    $("#TreatmTable select").each(function () {
+        $(this).width($(this).closest('td').width());
+    });
+}
