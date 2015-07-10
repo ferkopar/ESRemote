599a9427bf7b3cac7f20a971a9d710f08cddfc73
ferkopar <ferkopa@gmail.com>
2015-07-10 16:52:49 +0200

    P3000.js

--- EsToDoWeb/Scripts/ApexScripts/P3000.js
+++ EsToDoWeb/Scripts/ApexScripts/P3000.js
@@ -1,4 +1,6 @@
 ﻿function goToEdit(p) {
-
-    window.location = 'f?p=&APP_ID.:3001:&SESSION.:::NO::P3001_SUBJ_ID:' +p;
+    var urn = 'f?p=&APP_ID.:3001:&SESSION.::NO::P3001_SUBJ_ID:' + p;
+    //f?p=App:Page:Session:Request:Debug:ClearCache:itemNames:itemValues:PrinterFriendly
+    alert(urn);
+    window.location = urn;
 }
