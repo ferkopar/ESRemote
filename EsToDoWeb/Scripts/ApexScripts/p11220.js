d841e8ba79b919c355f488fc067465696cb8d149
ferkopar <ferkopa@gmail.com>
2015-08-11 13:59:52 +0200

    p11220.js

--- EsToDoWeb/Scripts/ApexScripts/p11220.js
+++ EsToDoWeb/Scripts/ApexScripts/p11220.js
@@ -13,6 +13,22 @@ function idChange(p) {
     dirty(p);
 }
 
+function Clear(){
+    var roleLevel = $("#ROLL_TYPE").children().val();
+    $("input").each(function () {
+        $(this).val("");
+    });
+    $("#ROLL_TYPE").children().val(roleLevel);
+}
+
+function CreateChild(){
+    var groupID   = $("#SUPER_TYP_ID").children().val();
+    var groupName = $("#SUPER_TYPE_NAME").children().val()
+    Clear();
+    $("#GROUP_ID").children().val(groupID);
+    $("#SUPER_GROUP_NAME").children().val(groupName);
+}
+
 function SaveChanges() {
     if (!dirty) return;
     $this = $(this);
@@ -21,33 +37,48 @@ function SaveChanges() {
     jsonTxt += formatJsonKeyValuePairStr("SUPER_TYPE_NAME", $("#SUPER_TYPE_NAME").children().val());
     jsonTxt += formatJsonKeyValuePairStr("SUPER_TYPE_SHORT_NAME", $("#SUPER_TYPE_SHORT_NAME").children().val());
     jsonTxt += formatJsonKeyValuePair("GROUP_ID", $("#GROUP_ID").children().val());
-    jsonTxt += formatJsonKeyValuePair("ROLL_TYPE", $("#ROLL_TYPE").children().val());
-
+    jsonTxt += formatJsonKeyValuePairNoComma("ROLL_TYPE", $("#ROLL_TYPE").children().val());
+    var translateDirty = false;
     if ($("#translateTable>tbody tr").length > 0) {
-        jsonTxt += '"TREATM_PARAMS" : [';
         $("#translateTable>tbody tr").each(function () {
             $this = $(this);
             editField = $this.children(".text").children();
-
-            var inputValue = editField.val();
-            var inputTagName = editField.prop("tagName");
-            var paramId = $this.children(".rowHead").children(".paramId").val();
-            var inputParamType = $this.children(".rowHead").children(".paramType").val();
-            jsonTxt += "{";
-            //jsonTxt += formatJsonKeyValuePairStr("TEXT", isEmpty(inputValue) ? 0 : inputValue);
-            jsonTxt += formatJsonKeyValuePairStr("TEXT", editField.val());
-            jsonTxt = jsonTxt.substring(0, jsonTxt.length - 1);
-            jsonTxt += "}\n,";
-
+            if (editField.hasClass("dirty")) {
+                translateDirty = true;
+            }
+        });
+    }
+    if (translateDirty) {
+        jsonTxt += ',"TRANSALTES" : [';
+        $("#translateTable>tbody tr").each(function () {
+            $this = $(this);
+            editField = $this.children(".text").children();
+            if (editField.hasClass("dirty")) {
+                var inputValue = editField.val();
+                var inputTagName = editField.prop("tagName");
+                var paramId = $this.children(".rowHead").children(".paramId").val();
+                var inputParamType = $this.children(".rowHead").children(".paramType").val();
+                jsonTxt += "{";
+                jsonTxt += formatJsonKeyValuePair("LANGUAGE_ID", $this.data("langid"));
+                jsonTxt += formatJsonKeyValuePairNoCommaStr("TEXT", editField.val());
+                jsonTxt += "}\n,";
+                editField.removeClass("dirty");
+            }
         });
         jsonTxt = jsonTxt.substring(0, jsonTxt.length - 1);
-        jsonTxt += "],\n";
-        jsonTxt = jsonTxt.substring(0, jsonTxt.length - 1);
-
-        jsonTxt += "}\n";
-
-        alert(jsonTxt);
+        jsonTxt += "]\n,";
     }
+    jsonTxt = jsonTxt.substring(0, jsonTxt.length - 1);
+    jsonTxt += "}\n";
+    $s("P11220_DEBUG", jsonTxt);
+//    var get = new htmldb_Get(null,&APP_ID.,'APPLICATION_PROCESS=SaveChanges',11220);
+//    get.addParam("x01",jsonTxt); 
+//    var gReturn = get.get();
+////    alert(gReturn);
+//    get = null;
+//    apex.submit();
+
+    //alert(jsonTxt);
 }
 
 
